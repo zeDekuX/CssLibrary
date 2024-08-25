78423bda1a504219ea3bd696b382439de9855c54
zeDekuX <118761474+zeDekuX@users.noreply.github.com>
2024-08-25 17:43:16 +0200

    Update TextHacked.js

--- JS/TextHacked.js
+++ JS/TextHacked.js
@@ -1,1 +1,31 @@
 
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+          let interval = null;
+
+          document.querySelector("#hacked").onmouseover = event => {  
+            let iteration = 0;
+            
+            clearInterval(interval);
+            
+            interval = setInterval(() => {
+              event.target.innerText = event.target.innerText
+                .split("")
+                .map((letter, index) => {
+                  if(index < iteration) {
+                    return event.target.dataset.value[index];
+                  }
+                
+                  return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+              
+              if(iteration >= event.target.dataset.value.length){ 
+                clearInterval(interval);
+              }
+              
+              iteration += 1 / 3;
+            }, 30);
+          }
+          
+//inserire id="hacked" al tag cui effettuare l'effetto
